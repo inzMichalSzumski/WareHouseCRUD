37566b895ab483a4b1087e94ff3d7ca743748ce1
sszumal@gmail.com <sszumal@gmail.com>
2019-08-15 03:14:00 +0200

    cleaning

--- WarehouseCRUD/MainWindow.cs
+++ WarehouseCRUD/MainWindow.cs
@@ -26,85 +26,12 @@ namespace WarehouseCRUD
             DocumentsList.DoubleClick += new EventHandler(DocumentsList_MouseDoubleClick);
         }
 
-        private void DBInitializer(List<WareHouseDocument> dataFeed)
-        {
-            using (var db = new WareHouseContext())
-            {
-                db.WareHouseDocuments.AddRange(dataFeed);
-                db.SaveChanges();
-
-                var down = from whdset in db.WareHouseDocuments
-                           select whdset;
-
-                var data = down.ToList();
-                
-            }
-        }
-
-        public List<WareHouseDocument> PopulateDocumentsList()
+        public void PopulateDocumentsList()
         {
             DocumentsList.DisplayMember = "DocumentName";
             List<WareHouseDocument> MyList = new List<WareHouseDocument>();
             MyList = dBService.GetAllDocuments();
             DocumentsList.DataSource = MyList;
-
-            
-
-            return MyList;
-
-            //MyList.Add(new WareHouseDocument
-            //{
-            //    DocumentName = "doc1",
-            //    ClientNumber = 123,
-            //    Created = new DateTime(2019, 08, 01),
-            //    WareHouseDocumentId = Guid.NewGuid(),
-            //    Items = new List<Item>
-            //    {
-            //        new Item
-            //        {
-            //            Amount = 3,
-            //            BruttoPrice = 5.50M,
-            //            Name = "mlotek",
-            //            ItemId = Guid.NewGuid()
-            //        },
-
-            //        new Item
-            //        {
-            //            Amount = 5,
-            //            BruttoPrice = 3.90M,
-            //            Name = "gwozdz",
-            //            ItemId = Guid.NewGuid()
-            //        }
-            //    }
-            //});
-            //MyList.Add(new WareHouseDocument
-            //{
-            //    DocumentName = "doc2",
-            //    ClientNumber = 456,
-            //    Created = new DateTime(2019, 08, 05),
-            //    WareHouseDocumentId = Guid.NewGuid(),
-            //    Items = new List<Item>
-            //    {
-            //        new Item
-            //        {
-            //            Amount = 1,
-            //            BruttoPrice = 5.50M,
-            //            Name = "mlotek",
-            //            ItemId = Guid.NewGuid()
-            //        },
-
-            //        new Item
-            //        {
-            //            Amount = 10,
-            //            BruttoPrice = 3.90M,
-            //            Name = "gwozdz",
-            //            ItemId = Guid.NewGuid()
-            //        }
-            //    }
-
-            //});
-
-
         }
 
         private void DocumentsList_SelectedIndexChanged(object sender, EventArgs e)
